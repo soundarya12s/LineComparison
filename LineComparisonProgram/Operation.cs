43e3ba009db935158ff20e9045293c0f0176abb6
S Soundarya <soundarya.s@tvsnext.com>
2023-07-12 11:10:15 +0530

    [Soundarya] Add: Added length Caculation

---
+++ LineComparisonProgram/Operation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProgram
+{
+  public class Operation
+    {
+        int x1, x2, y1, y2;
+        public Operation(int x1, int x2, int y1, int y2) {
+        
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public void CalculateLength()
+        {
+            double length = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
+            Console.WriteLine("Length of a line" + length);
+        }
+    } 
+}
--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -6,6 +6,8 @@ class Program
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Line Comparison Problem statement");
+            Operation op = new Operation(0,2,0,4);
+            op.CalculateLength();
         }
     }
 
