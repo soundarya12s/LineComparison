f9e1979184fc2e5f30ac3af0da590f3c15c6f9e1
S Soundarya <soundarya.s@tvsnext.com>
2023-07-12 10:59:07 +0530

    [Soundarya]Add:Folder structure created.

---
+++ LineComparisonProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisonProgram {
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Problem statement");
+        }
+    }
+
+}
